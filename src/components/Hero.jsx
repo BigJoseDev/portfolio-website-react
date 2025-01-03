c6242370782b3a2fbba978c1730ab57ec7d2f8f9
Jose <aliemhejosemaria@gmail.com>
2025-01-03 22:59:46 +0100

    completed framer motion

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,17 +23,17 @@ const Hero = () => {
             animate='visible'
 
             className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-7xl">
-              BigJoseDev
+             Josemaria Aliemhe
             </motion.h1>
             <motion.span
-            variants={container(0)}
+            variants={container(0.5)}
             initial='hidden'
             animate='visible'
             className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
               Frontend Developer
             </motion.span>
             <motion.p
-            variants={container(0)}
+            variants={container(1)}
             initial='hidden'
             animate='visible'
             className="my-2 max-w-xl py-6 font-light tracking-tighter">
@@ -43,7 +43,11 @@ const Hero = () => {
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
           <div className="flex justify-center">
-            <img src={profilePic} alt="" />
+            <motion.img
+            initial={{x:100, opacity: 0}}
+            animate={{x:0, opacity:1}}
+            transition={{duration: 1, delay: 1.2}}
+            src={profilePic} alt="" />
           </div>
         </div>
       </div>

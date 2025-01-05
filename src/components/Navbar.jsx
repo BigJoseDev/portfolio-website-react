72ca7af04e242e55ae46afdb0f6750f6f7aee813
Jose <aliemhejosemaria@gmail.com>
2025-01-05 16:14:38 +0100

    changes linkedin

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <nav className="mb-10 flex items-center justify-between py-6">
       <div className="flex items-center justify-center gap-4 text-2xl">
-        <a href="https://www.linkedin.com/in/your-profile" target="_blank" rel="noopener noreferrer">
+        <a href="http://linkedin.com/in/josemaria-aliemhe-789281274" target="_blank" rel="noopener noreferrer">
           <FaLinkedin />
         </a>
         <a href="https://github.com/BigJoseDev" target="_blank" rel="noopener noreferrer">
